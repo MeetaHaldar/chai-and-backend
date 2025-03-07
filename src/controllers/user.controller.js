d1a73bb32c5e3bf07c391eca26045c32c76aa15d
MeetaHaldar <meetahaldar1001@gmail.com>
2025-03-07 13:24:44 +0530

    algirthm for user data save

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,13 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-export const registerUser = asyncHandler(async (req, res, next) => {});
+export const registerUser = asyncHandler(async (req, res, next) => {
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // create user object - create entry in db
+  // check for user creation
+  // return res
+});
 
 // export default registerUser;
